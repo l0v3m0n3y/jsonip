5d08a2b133bf536a787f13f965bb061cb0336e3d
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:18:27 +0300

    refactor: remove jsonip.sh from root (moved to src/)

--- jsonip.sh
+++
@@ -1,8 +0,0 @@
-api="https://jsonip.com"
-user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
-function self_ip() {
-		curl --request GET \
-		--url "$api/" \
-		--user-agent "$user_agent" \
-		--header "accept: application/json"
-}
